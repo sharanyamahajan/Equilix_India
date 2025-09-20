a101978f08266e11a5f710b11cc368c0b15ec57c
Sharanya Mahajan <sharanyamahajan10@gmail.com>
2025-09-20 17:10:11 +0000

    Try fixing this error: `Console Error: Invalid DOM property `stroke-widt

--- src/components/icons/equilix-logo.tsx
+++ src/components/icons/equilix-logo.tsx
@@ -13,51 +13,51 @@ export function EquilixLogo(props: SVGProps<SVGSVGElement>) {
       <path
         d="M17.5 10.5C17.5 12.9853 15.4853 15 13 15C11.5 15 10.5 14.5 9 13.5"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M6.5 10.5C6.5 12.9853 8.51472 15 11 15C12.5 15 13.5 14.5 15 13.5"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12 15V19C12 20.1046 12.8954 21 14 21H15"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12 15V19C12 20.1046 11.1046 21 10 21H9"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12 11C12 9.34315 10.6569 8 9 8C7.34315 8 6 9.34315 6 11"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M13 11C13 9.34315 14.3431 8 16 8C17.6569 8 19 9.34315 19 11"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12 11V3C12 2.44772 12.4477 2 13 2C13.5523 2 14 2.44772 14 3V6.34146C14 6.34146 16.5 5.5 17 8"
         stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
