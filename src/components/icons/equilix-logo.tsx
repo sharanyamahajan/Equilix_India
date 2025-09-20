3b019a3d5f05b2fd3bfa8c46825a8a3696c60bda
Sharanya Mahajan <sharanyamahajan10@gmail.com>
2025-09-20 17:09:33 +0000

    i step more

--- src/components/icons/equilix-logo.tsx
+++ src/components/icons/equilix-logo.tsx
@@ -11,25 +11,53 @@ export function EquilixLogo(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        d="M12 2C6.477 2 2 6.477 2 12C2 17.523 6.477 22 12 22C17.523 22 22 17.523 22 12C22 6.477 17.523 2 12 2Z"
+        d="M17.5 10.5C17.5 12.9853 15.4853 15 13 15C11.5 15 10.5 14.5 9 13.5"
         stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
       <path
-        d="M7 14C8.5 16,10.5 17,12 17C13.5 17,15.5 16,17 14"
+        d="M6.5 10.5C6.5 12.9853 8.51472 15 11 15C12.5 15 13.5 14.5 15 13.5"
         stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
       <path
-        d="M8 9.5C9 10.5,10.5 11,12 11C13.5 11,15 10.5,16 9.5"
+        d="M12 15V19C12 20.1046 12.8954 21 14 21H15"
         stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 15V19C12 20.1046 11.1046 21 10 21H9"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 11C12 9.34315 10.6569 8 9 8C7.34315 8 6 9.34315 6 11"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13 11C13 9.34315 14.3431 8 16 8C17.6569 8 19 9.34315 19 11"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 11V3C12 2.44772 12.4477 2 13 2C13.5523 2 14 2.44772 14 3V6.34146C14 6.34146 16.5 5.5 17 8"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   );
